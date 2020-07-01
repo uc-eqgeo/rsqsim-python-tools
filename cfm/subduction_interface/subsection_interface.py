e760daa89f081816587113e3c549f986288fb249
arh79 <arh79@cam.ac.uk>
2020-07-02 11:39:00 +1200

    Spacing of points for subduction interface polygons

--- cfm/subduction_interface/subsection_interface.py
+++ cfm/subduction_interface/subsection_interface.py
@@ -1,6 +1,26 @@
 from icp_error.io.array_operations import read_tiff
 import geopandas as gpd
 import numpy as np
+from shapely.geometry import LineString
+
+
+def nan_helper(a):
+    """Helper to handle indices and logical indices of NaNs.
+
+    Input:
+        - y, 1d numpy array with possible NaNs
+    Output:
+        - nans, logical indices of NaNs
+        - index, a function, with signature indices= index(logical_indices),
+          to convert logical indices of NaNs to 'equivalent' indices
+    Example:
+        >>> # linear interpolation of NaNs
+        >>> nans, b= nan_helper(a)
+        >>> b[nans]= np.interp(x(nans), x(~nans), y[~nans])
+    """
+
+    return np.isnan(a), lambda b: b.nonzero()[0]
+
 
 x, y, z = read_tiff("hik_res_0.01_lt50_nztm.tif")
 z *= 1000
@@ -23,6 +43,49 @@ across_dists = (x_grid - corner[0]) * across_vec[0] + (y_grid - corner[1]) * acr
 
 profile_half_width = 1000
 profile_spacing = 7000
+
+# Find start location
+start_along = min(along_dists[~np.isnan(z)])
+end_along = max(along_dists[~np.isnan(z)])
+
+along_spaced = np.arange(start_along + profile_spacing/2, end_along, profile_spacing)
+
+for along in along_spaced:
+    row_end = corner + along * along_overall
+
+    along_min = along - profile_half_width
+    along_max = along + profile_half_width
+
+    in_swath = np.logical_and(along_dists >= along_min, along_dists <= along_max)
+    swath_across = across_dists[in_swath]
+    swath_z = z[in_swath]
+
+    across_no_nans = swath_across[~np.isnan(swath_z)]
+
+    start_across = min(across_no_nans)
+    end_across = max(across_no_nans)
+
+    initial_spacing = np.arange(start_across, end_across, profile_half_width)
+
+    across_vs_z = np.vstack((across_no_nans, swath_z[~np.isnan(swath_z)])).T
+    sorted_coords = across_vs_z[across_vs_z[:, 0].argsort()]
+
+    interp_z = np.interp(initial_spacing, sorted_coords[:, 0], sorted_coords[:, 1])
+
+    interp_line = LineString(np.vstack((initial_spacing, interp_z)).T)
+
+    interpolation_distances = np.arange(profile_spacing/2, interp_line.length, profile_spacing)
+    interpolated_points = [interp_line.interpolate(distance) for distance in interpolation_distances]
+
+    interpolated_x = np.array([point.x for point in interpolated_points])
+    interpolated_z_values = np.array([point.y for point in interpolated_points])
+
+    point_xys = np.array([row_end + across_i * across_vec for across_i in interpolated_x])
+    point_xyz = np.vstack((point_xys.T, interpolated_z_values)).T
+
+
+
+
 
 
 
