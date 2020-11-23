837f0785051b2eb5da3620fd9f6fc64142bf025a
Wooneh <sam.woon@hotmail.com>
2020-11-24 11:48:37 +1300

    Create environment file for conda

--- src/rsqsim_api/setup.py
+++ src/rsqsim_api/setup.py
@@ -1,14 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='rsqsim-api',
       version='0.1',
       description='Read and write of RSQSim inputs and outputs',
       author='Andy Howell and several others hopefully',
       author_email='a.howell@gns.cri.nz',
-      packages=['rsqsim_api'],
-      install_requires=["numpy>=1.18.1",
-                        "geopandas>=0.6.1",
-                        "ipyvolume>=0.5.2",
-                        "jupyter-client>=6.1.7",
-                        "matplotlib>=3.1.1"],
+      packages=find_packages(),
       )
