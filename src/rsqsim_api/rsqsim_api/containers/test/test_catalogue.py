7bd9eab451d10ad77476ff1d463d1517cdfb70f1
Wooneh <sam.woon@hotmail.com>
2020-11-27 13:05:49 +1300

    Created test file for catalogue

---
+++ src/rsqsim_api/rsqsim_api/containers/test/test_catalogue.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+import pandas as pd
+from rsqsim_api.containers.catalogue import RsqSimCatalogue
+
+TESTDATA_FILE = os.path.join(os.path.dirname(__file__), 'data/eqs..out')
+
+
+class TestReadCatalogue(unittest.TestCase):
+    def setUp(self):
+        self.catalogue = RsqSimCatalogue()
+
+    def test_from_catalogue_file(self):
+        catalogue = self.catalogue.from_catalogue_file(TESTDATA_FILE)
+        pd.testing.assert_frame_equal(catalogue.catalogue_df,
+                                      pd.DataFrame({'t0': [668092957.37445151805877685547, 741329352.47262990474700927734],
+                                                    'm0': [12597022496058978.000000, 121238811568268368.000000],
+                                                    'mw': [4.666845, 5.322428],
+                                                    'x': [192217.149509, 195970.303394],
+                                                    'y': [5008104.808942, 5005539.900412],
+                                                    'z': [-3200.0, -11700.0],
+                                                    'area': [843196.824748, 5283251.259523],
+                                                    'dt': [0.497987, 1.352069]}))
