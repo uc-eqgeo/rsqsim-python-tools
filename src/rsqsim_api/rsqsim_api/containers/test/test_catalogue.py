5b38874d3659a6f8fb342fe91f9d911c510b2160
Wooneh <sam.woon@hotmail.com>
2020-12-01 10:19:24 +1300

    Added test for reading csv

--- src/rsqsim_api/rsqsim_api/containers/test/test_catalogue.py
+++ src/rsqsim_api/rsqsim_api/containers/test/test_catalogue.py
@@ -3,15 +3,14 @@ import os
 import pandas as pd
 from rsqsim_api.containers.catalogue import RsqSimCatalogue
 
-TESTDATA_FILE = os.path.join(os.path.dirname(__file__), 'data/eqs..out')
-
 
 class TestReadCatalogue(unittest.TestCase):
     def setUp(self):
         self.catalogue = RsqSimCatalogue()
 
     def test_from_catalogue_file(self):
-        catalogue = self.catalogue.from_catalogue_file(TESTDATA_FILE)
+        catalogue = self.catalogue.from_catalogue_file(
+            os.path.join(os.path.dirname(__file__), 'data/eqs..out'))
         pd.testing.assert_frame_equal(catalogue.catalogue_df,
                                       pd.DataFrame({'t0': [668092957.37445151805877685547, 741329352.47262990474700927734],
                                                     'm0': [12597022496058978.000000, 121238811568268368.000000],
@@ -21,3 +20,12 @@ class TestReadCatalogue(unittest.TestCase):
                                                     'z': [-3200.0, -11700.0],
                                                     'area': [843196.824748, 5283251.259523],
                                                     'dt': [0.497987, 1.352069]}))
+
+    def test_from_csv_and_arrays(self):
+        catalogue = self.catalogue.from_csv_and_arrays(
+            os.path.join(os.path.dirname(__file__), 'data/bruce_m7/bruce_m7_10kyr'))
+        self.assertIsNotNone(catalogue.catalogue_df)
+        self.assertIsNotNone(catalogue.event_list)
+        self.assertIsNotNone(catalogue.patch_list)
+        self.assertIsNotNone(catalogue.patch_slip)
+        self.assertIsNotNone(catalogue.patch_time_list)
