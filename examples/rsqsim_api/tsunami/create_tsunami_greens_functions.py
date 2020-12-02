Coarser grid for GFs, fix problem with dip calculation, print patch number to give indicator of progress when calculating GFs

examples/rsqsim_api/tsunami/create_tsunami_greens_functions.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     faults = RsqSimMultiFault.read_fault_file_bruce("/home/UOCNT/arh128/PycharmProjects/rnc2/data/bruce_m7/bruce_faults.in",
                                                     "/home/UOCNT/arh128/PycharmProjects/rnc2/data/bruce_m7/bruce_fault_names.in",
                                                     transform_from_utm=True)
-
-    x_data = np.arange(800000, 1751000, 1000)
-    y_data = np.arange(5200000, 5601000, 1000)
+    # All of NZ
+    x_data = np.arange(800000, 2200000, 2000)
+    y_data = np.arange(4200000, 6020000, 2000)
     x_grid, y_grid = np.meshgrid(x_data, y_data)
 
-    multiprocess_gf_to_hdf(faults, x_grid, y_grid, out_file="tsunami_gfs.hdf5")
+    multiprocess_gf_to_hdf(faults.faults[0], x_grid, y_grid, out_file="test.hdf5")
 

src/rsqsim_api/rsqsim_api/containers/fault.py
@@ -836,7 +836,7 @@ class RsqSimTriangularPatch(RsqSimGenericPatch):
 
     def calculate_dip(self):
         horizontal = np.linalg.norm(self.down_dip_vector[:-1])
-        vertical = -1 * self.down_dip_vector[:-1]
+        vertical = -1 * self.down_dip_vector[-1]
         return np.degrees(np.arctan(vertical / horizontal))
 
     @property