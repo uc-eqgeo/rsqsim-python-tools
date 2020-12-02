2c4f06c11837c6a64ea140ddca383f6c9f8b39b4
andy22b <andy22b@gmail.com>
2020-12-02 14:17:38 +1300

    Add name==main to allow multiprocessing to function correctly

--- examples/rsqsim_api/tsunami/create_tsunami_greens_functions.py
+++ examples/rsqsim_api/tsunami/create_tsunami_greens_functions.py
@@ -5,12 +5,14 @@ import numpy as np
 from rsqsim_api.tsunami.tsunami_multiprocessing import multiprocess_gf_to_hdf
 from rsqsim_api.containers.fault import RsqSimMultiFault
 
+if __name__ == "__main__":
+    faults = RsqSimMultiFault.read_fault_file_bruce("/home/UOCNT/arh128/PycharmProjects/rnc2/data/bruce_m7/bruce_faults.in",
+                                                    "/home/UOCNT/arh128/PycharmProjects/rnc2/data/bruce_m7/bruce_fault_names.in",
+                                                    transform_from_utm=True)
 
-jkn = pickle.load(open("jordan_kek_needles.pkl", "rb"))
+    x_data = np.arange(800000, 1751000, 1000)
+    y_data = np.arange(5200000, 5601000, 1000)
+    x_grid, y_grid = np.meshgrid(x_data, y_data)
 
-x_data = np.arange(1400000, 1751000, 1000)
-y_data = np.arange(5200000, 5601000, 1000)
-x_grid, y_grid = np.meshgrid(x_data, y_data)
-
-multiprocess_gf_to_hdf(jkn, x_grid, y_grid, out_file="../test.hdf5")
+    multiprocess_gf_to_hdf(faults, x_grid, y_grid, out_file="tsunami_gfs.hdf5")
 
