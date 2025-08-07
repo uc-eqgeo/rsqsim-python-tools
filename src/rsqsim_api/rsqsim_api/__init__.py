ab7a5211acee3d4c4e13871eb69a9464d8c47720
arh128 <a.howell@gns.cri.nz>
2025-08-07 14:55:36 +1200

    Restructure for toml

--- src/rsqsim_api/rsqsim_api/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Tools for creating, reading and plotting RSQSim input and output files
-Modules:
-    * catalogue - A module for reading and manipulating RSQSim catalogue data
-    * fault - module for creating and reading fault meshes
-"""
---
+++ src/setup_old.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='rsqsim-api',
+      version='0.0.1',
+      description='Read and write of RSQSim inputs and outputs',
+      author='Andy Howell and several others hopefully',
+      author_email='a.howell@gns.cri.nz',
+      packages=find_packages(),
+      package_data={'rsqsim_api': ['visualisation/data/*', 'visualisation/data/*/*']},
+      )
